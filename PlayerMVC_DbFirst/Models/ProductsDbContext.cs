bc563920c5dc66138925efe527d30246dd81480e
BhavanaRamyaBanala <ramyabanala12@gmail.com>
2024-02-06 15:51:36 +0530

    First commit

---
+++ PlayerMVC_DbFirst/Models/ProductsDbContext.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PlayerMVC_DbFirst.Models
+{
+    public partial class ProductsDbContext : DbContext
+    {
+        public ProductsDbContext()
+        {
+        }
+
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; } = null!;
+        public virtual DbSet<Player> Players { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=DESKTOP-M3A0U1O;database=ProductsDb;trusted_connection=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.ToTable("Book");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Author).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Player>(entity =>
+            {
+                entity.Property(e => e.PlayerId).ValueGeneratedNever();
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Team)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(e => e.Pid)
+                    .HasName("PK__Product__C577554074327322");
+
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Pid)
+                    .ValueGeneratedNever()
+                    .HasColumnName("PId");
+
+                entity.Property(e => e.PmfDate)
+                    .HasColumnType("date")
+                    .HasColumnName("PMfDate");
+
+                entity.Property(e => e.Pname).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
